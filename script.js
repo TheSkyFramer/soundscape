897d8eadb7566f3fb745484b3b2ef8075f748a27
TheSkyFramer <159212091+TheSkyFramer@users.noreply.github.com>
2024-04-02 19:53:40 +0400

    main files

---
+++ script.js
@@ -0,0 +1,141 @@
+document.getElementById('image-1').addEventListener('click', function() {
+    var audio = document.getElementById('audio');
+    var audio2 = document.getElementById('audio1');
+
+    if (audio.paused) {
+        if(!audio2.paused){
+            audio2.pause();
+        }
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+document.getElementById('image-2').addEventListener('click', function() {
+    var audio = document.getElementById('audio');
+    var audio2 = document.getElementById('audio1');
+    if (audio.paused) {
+        if(!audio2.paused){
+            audio2.pause();
+        }
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+document.getElementById('image-3').addEventListener('click', function() {
+    var audio = document.getElementById('audio');
+    var audio2 = document.getElementById('audio1');
+    if (audio.paused) {
+        if(!audio2.paused){
+            audio2.pause();
+        }
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+document.getElementById('image-4').addEventListener('click', function() {
+    var audio = document.getElementById('audio');
+    var audio2 = document.getElementById('audio1');
+    if (audio.paused) {
+        if(!audio2.paused){
+            audio2.pause();
+        }
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+
+document.getElementById('image-5').addEventListener('click', function() {
+    var audio = document.getElementById('audio1');
+    var audio2 = document.getElementById('audio');
+    if (audio.paused) {
+        if(!audio2.paused){
+            audio2.pause();
+        }
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+
+
+document.getElementById('image-6').addEventListener('click', function() {
+    var audio = document.getElementById('audio');
+    var audio2 = document.getElementById('audio1');
+    var audio3 = document.getElementById('audio2');
+    if (audio3.paused) {
+        if(!audio.paused){
+            audio.pause();
+        }
+        if(!audio2.paused){
+            audio2.pause();
+        }
+
+        audio3.play();
+    } else {
+        audio3.pause();
+    }
+});
+
+
+document.getElementById('image-7').addEventListener('click', function() {
+    var audio = document.getElementById('audio');
+    var audio2 = document.getElementById('audio1');
+    var audio3 = document.getElementById('audio2');
+    var audio4 = document.getElementById('audio3');
+
+    if (audio4.paused) {
+        if(!audio.paused){
+            audio.pause();
+        }
+        if(!audio2.paused){
+            audio2.pause();
+        }
+        if(!audio3.paused){
+            audio3.pause();
+        }
+        audio4.play();
+    } else {
+        audio4.pause();
+    }
+});
+
+
+
+// document.getElementById('image-5').addEventListener('click', function() {
+//     var audio = document.getElementById('audio1');
+//     var audio2 = document.getElementById('audio1');
+//     if (audio.paused) {
+//         audio.play();
+//     } else {
+//         audio.pause();
+//     }
+// });
+
+// document.getElementById('image-6').addEventListener('click', function() {
+//     var audio = document.getElementById('audio2');
+//     var audio2 = document.getElementById('audio2');
+//     if (audio.paused) {
+//         audio.play();
+//     } else {
+//         audio.pause();
+//     }
+// });
+
+// document.getElementById('image-7').addEventListener('click', function() {
+//     var audio = document.getElementById('audio3');
+//     var audio2 = document.getElementById('audio3');
+//     if (audio.paused) {
+//         audio.play();
+//     } else {
+//         audio.pause();
+//     }
+// });
